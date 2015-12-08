64a76e890038d7eb5ae499378fe498bad31acf67
Shomron Sougang Kamgang <sskamgang@genetec.com>
2015-12-08 10:20:11 -0500

    day 4

---
+++ src/AdventCoins_d4/AdventCoins_d4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCoins_d4
+{
+    class Program
+    {
+        static MD5 m_d5 = MD5.Create();
+        static void Main(string[] args)
+        {
+            var entrySource = "ckczppom";
+
+            bool found = false;
+            int index = -1;
+            while (!found)
+            {
+                index++;
+                found = IsFound(GenerateHash(entrySource + index));
+            }
+
+            Console.WriteLine(index);
+            Console.ReadKey();
+        }
+
+        private static string GenerateHash(string s)
+        {
+            var result = m_d5.ComputeHash(Encoding.UTF8.GetBytes(s));
+            var hexa = "";
+            result.ToList().ForEach(x => hexa += x.ToString("X2"));
+            return hexa;
+        }
+
+        private static bool IsFound(string generateHash)
+        {
+            return generateHash.StartsWith("000000");
+        }
+    }
+}
